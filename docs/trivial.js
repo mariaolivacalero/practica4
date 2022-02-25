d878a293863b0be40439eddd60929ad8a5665acd
mariaolivacalero <201903173@alu.comillas.edu>
2022-02-25 17:16:18 +0100

    changes

---
+++ docs/trivial.js
@@ -0,0 +1,52 @@
+let url = 'https://opentdb.com/api.php?amount=1&type=boolean&category=';
+
+let = res = {}
+let getQuestions= async (categoria, nivel) => {
+    link = url + categoria + "&difficulty=" + nivel; 
+    console.log(link);
+    let response = await fetch(link);
+    if (response.ok){
+        res = await response.json();
+        console.log(res.results);
+        document.getElementById('question').innerHTML = res.results[0].question;
+        document.getElementById('category').innerHTML = res.results[0].category;
+
+    }
+
+}
+
+
+function getOption() {
+    selectCategory = document.querySelector('#select1');
+    selectDifficulty = document.querySelector('#select2');
+    categoria = selectCategory.value;
+    nivel =  selectDifficulty.value;
+    console.log(categoria)
+    correct= getQuestions(categoria,nivel);
+    document.getElementById('selector').hidden= true;
+    document.getElementById('juego').hidden= false;
+}
+
+
+function guess(){
+    let options = document.getElementsByName('options');
+    for(i = 0; i < options.length; i++) {
+        if(options[i].checked){
+            console.log(options[i].value)
+            if (options[i].value === res.results[0].correct_answer) {
+                result.innerHTML = "That's the correct answer!:)";
+              } else
+                result.innerHTML = 'Sorry! :( You can try again';
+            }
+        }
+        
+
+}
+
+
+
+
+function start(){
+    document.getElementById('selector').hidden= false;
+    document.getElementById('juego').hidden= true;
+}
